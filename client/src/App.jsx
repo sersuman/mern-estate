5aed1886adf3ca6a067e09e317fe6f81eb1c824d
sersuman <sumanshrestha670@gmail.com>
2023-12-05 08:42:14 +0545

    pagination: react-router-dom setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/profile",
+    element: <Profile />
+  },
+  {
+    path: "/sign-in",
+    element: <Signin />
+  },
+  { 
+    path: "/sign-up",
+    element: <Signup />
+  }
+])
 export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <RouterProvider router={router} />
   )
 }
